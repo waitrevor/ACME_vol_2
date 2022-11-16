6b7e5b4092fb7abfea50c493769af432f04b5e62
waitrevor <trevorw5@student.byu.edu>
2022-11-16 11:46:37 -0700

    Finished Problems 1-6

--- UnixShell2/shell2.py
+++ UnixShell2/shell2.py
@@ -1,10 +1,14 @@
 # shell2.py
 """Volume 3: Unix Shell 2.
-<Name>
-<Class>
-<Date>
+<Name> Trevor Wai
+<Class> Section 2
+<Date> 11/16/22
 """
 
+import os
+from glob import glob
+import subprocess
+import numpy as np
 
 # Problem 3
 def grep(target_string, file_pattern):
@@ -21,7 +25,17 @@ def grep(target_string, file_pattern):
         matched_files (list): list of the filenames that matched the file
                pattern AND the target string.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #Initialize Variables use glob to find the files within the directory
+    matched_files = []
+    match_pattern = glob(f'**/{file_pattern}', recursive=True)
+
+    #Loops through the files and looks for the target string in the files.
+    for file in match_pattern:
+        with open(file, 'r') as infile:
+            if target_string in infile.read():
+                matched_files.append(file)
+
+    return matched_files
 
 
 # Problem 4
@@ -29,7 +43,19 @@ def largest_files(n):
     """Return a list of the n largest files in the current directory or its
     subdirectories (from largest to smallest).
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    #Searches and sorts the filenames
+    infiles = glob('**/*.*', recursive=True)
+    sizes = [os.path.getsize(file) for file in infiles]
+    infiles = list(np.array(infiles)[np.argsort(sizes)][::-1][:n])
+
+    #Counts the number of lines of the smallest file
+    num_lines = subprocess.check_output(['wc', '-1', infiles[-1]]).decode()
+
+    #Writes the number of lines to a file
+    with open('smallest.txt', 'w') as outfile:
+        outfile.write(num_lines[:num_lines.find(' ')])
+
+    return infiles
     
 # Problem 6    
 def prob6(n = 10):
